9d3f430228f68514beb6a1a32cd303c581bd3ec0
Renan Moreira <105755795+moreirarmcm@users.noreply.github.com>
2022-06-17 03:21:57 -0300

    Faturamento Doc
    Feita documentação do programa e alterações na exibição de resultados.

--- Faturamento.java
+++ Faturamento.java
@@ -1,24 +1,40 @@
 package EstudoJava;
 
 import java.util.Scanner;
-
+/**
+ * "Analisa Faturamento"
+ * 
+ * O programa recebe o faturamento diário de uma empresa e retorna:
+ * 1 - Qual foi o menor e o maior faturamento de um mês - ignorando valores nulos;
+ * 2 - Calcula a média de faturamentos durante o mês;
+ * 3 - Retorna a quantidade de dias que superaram a média em faturamento;
+ *  
+ * @author Renan
+ *
+ */
 public class Faturamento {
 	static double [] faturamento = new double[30];
-	static int dias_de_lucro = 0;
-	static double valor_maximo, valor_minimo,media_mensal = 0;
-	static int controle = 0;
+	static int dias_de_lucro = 0; // para calcular a média (incrementa sempre que faturamento > 0) e para calcular os dias que superaram a média.
+	static double valor_maximo, valor_minimo, media_mensal = 0;
+	static int controle = 0; //variável auxiliar.
+	
 	public static void main (String [] args) {
 		System.out.println("Digite os valores de faturamento do mês: ");
 		RecebeValores();
-		RetornaMaxMin();
-		RetornaMedia();
-		SuperandoMedia();
+		CalculaMaxMin();
+		CalculaMedia();
+		RetornaSuperMedia();
 	}
 
+	/**
+	 * Recebe os valores de faturamento usando um Scanner.
+	 * 
+	 * Trata exceção de digitação do ponto flutuante.
+	 */
 	public static void RecebeValores() {
 		Scanner sc = new Scanner(System.in);
 		for (int posicao_no_vetor = 0; posicao_no_vetor < faturamento.length; posicao_no_vetor++) {
-			System.out.print("Dia " + (posicao_no_vetor + 1) + " : ");
+			System.out.print("\nDia " + (posicao_no_vetor + 1) + " : ");
 			try {
 				faturamento[posicao_no_vetor] = sc.nextDouble();
 			}catch (java.util.InputMismatchException e) {
@@ -27,34 +43,42 @@ public class Faturamento {
 			if (faturamento[posicao_no_vetor] != 0) {
 				dias_de_lucro++;
 			}
-			System.out.println(faturamento[posicao_no_vetor] + " - " + dias_de_lucro);
 		}
 	}
-	
-	public static void RetornaMaxMin() {
+	/**
+	 * Realiza a verificação do vetor e retorna o maior e menor valor nas respectivas variáveis. Ignora os valores = 0.
+	 */
+	public static void CalculaMaxMin() {
 		valor_maximo = faturamento[0];
-		valor_minimo = Double.MAX_VALUE;
-		for (int verifica_maximo = 0; verifica_maximo < faturamento.length - 1; verifica_maximo++) {
-			if (faturamento[verifica_maximo] < faturamento[verifica_maximo + 1]) {
-				valor_maximo = faturamento[verifica_maximo + 1];
-			}else if (faturamento[verifica_maximo] != 0 && faturamento[verifica_maximo] < valor_minimo) {
-				valor_minimo = faturamento[verifica_maximo];
+		valor_minimo = Double.MAX_VALUE; //Tamanho máximo do tipo double;
+		for (int posicao_no_vetor = 0; posicao_no_vetor < faturamento.length - 1; posicao_no_vetor++) {
+			if (faturamento[posicao_no_vetor] < faturamento[posicao_no_vetor + 1]) {
+				valor_maximo = faturamento[posicao_no_vetor + 1]; // recebe o maior entre os dois valores
+			}else if (faturamento[posicao_no_vetor] != 0 && faturamento[posicao_no_vetor] < valor_minimo) {
+				valor_minimo = faturamento[posicao_no_vetor]; // recebe o menor valor, caso seja diferente de 0;
 			}
 		}
 	}
-	
-	public static void RetornaMedia() {
-		for (int calculando_media = 0; calculando_media < faturamento.length; calculando_media++) {
-			media_mensal += faturamento[calculando_media];
+	/**
+	 * Calcula a média, usando como denominador os dias onde o lucro foi diferente de 0.
+	 * 
+	 */
+	public static void CalculaMedia() {
+		for (int posicao_no_vetor = 0; posicao_no_vetor < faturamento.length; posicao_no_vetor++) {
+			media_mensal += faturamento[posicao_no_vetor];
 		}
-		media_mensal = media_mensal/dias_de_lucro;
+		media_mensal = media_mensal / dias_de_lucro;
 	}
 	
-	public static void SuperandoMedia() {
+	/**
+	 * Retorna o número de dias que superaram o faturamento médio.
+	 */
+	public static void RetornaSuperMedia() {
 		dias_de_lucro = 0;
 		for (int posicao_do_vetor = 0; posicao_do_vetor < faturamento.length; posicao_do_vetor++ ) {
 			controle = (faturamento[posicao_do_vetor] > media_mensal) ? dias_de_lucro++ : 0;
 		}
-		System.out.println(dias_de_lucro);
+		System.out.println("\n------//-----------//--------------//-----------");
+		System.out.println("Nesse mês a média de faturamento foi de: " + media_mensal + "\nO número de dias que superaram a média foi: " + dias_de_lucro);
 	}
 }
