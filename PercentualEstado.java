2677f663f01ef0be0f0b761c3a7d2b8d601fcdaf
Renan Moreira <105755795+moreirarmcm@users.noreply.github.com>
2022-06-17 03:25:34 -0300

    PercentualEstado doc
    Realizada documentação do programa.

--- PercentualEstado.java
+++ PercentualEstado.java
@@ -2,9 +2,14 @@ package EstudoJava;
 
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
-
+/**
+ * "Calcula Representação Percentual"
+ * O programa recebe o faturamento em 5 regiões e exibe o percentual de representação de cada região, arredondados.
+ * 
+ * @author Renan
+ *
+ */
 public class PercentualEstado {
-
 	public static void main(String[] args) {
 		double fat_sp = 67836.43;
 		double fat_rj = 36678.66;
@@ -12,9 +17,8 @@ public class PercentualEstado {
 		double fat_es = 27165.48;
 		double fat_other = 19849.53;
 		double fat_total = fat_sp + fat_rj + fat_mg + fat_es + fat_other; 
-		DecimalFormat df = new DecimalFormat("00.00"); 
+		DecimalFormat df = new DecimalFormat("00.00"); //para arredondar os valores
 		df.setRoundingMode(RoundingMode.UP);
-		
 		System.out.println("Percentuais de representação (por estado):" +
 				"\n  - SP - " + df.format(fat_sp/fat_total * 100) + "%" +
 				"\n  - RJ - " + df.format(fat_rj/fat_total * 100) + "%" +
@@ -22,5 +26,4 @@ public class PercentualEstado {
 				"\n  - ES - " + df.format(fat_es/fat_total * 100) + "%" +
 				"\n  - Outros - " + df.format(fat_other/fat_total * 100) + "%");
 	}
-
 }
